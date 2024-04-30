6a52bd27cad836838a636d9ffb89e25e4906d753
Kevin Zehrer <12397322+kzehrer@users.noreply.github.com>
2024-04-30 14:06:41 -0500

    Added claims to the home page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -11,6 +11,8 @@ public class IndexModel : PageModel
 
     public IReadOnlyDictionary<string, string> RequestHeaders { get { return Request.Headers.ToDictionary(h => h.Key, h => h.Value.ToString()); } }
 
+    public IReadOnlyDictionary<string, string> UserClaims { get { return User.Claims.ToDictionary(c => c.Type, c => c.Value); } }
+
     private readonly ILogger<IndexModel> _logger;
 
     public IndexModel(ILogger<IndexModel> logger)
